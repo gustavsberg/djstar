262762eb1047a762b4307df7ab9de745ce9051aa
Ihor <hainsgud@gmail.com>
2019-07-01 16:36:08 +0300

    basic styles and dividing html on blocks

--- djstar/urls.py
+++ djstar/urls.py
@@ -1,9 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from . import views
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('articles/', include('articles.urls')),
     path('about/', views.about),
 ]
+
+urlpatterns += staticfiles_urlpatterns()

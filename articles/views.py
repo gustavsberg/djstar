acc82aaae724c0393bce7d83a95b245254d372a4
Ihor <hainsgud@gmail.com>
2019-07-04 13:25:49 +0300

    adding author field and creation forms

--- articles/views.py
+++ articles/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Article
 from django.http import HttpResponse
 from django.contrib.auth.decorators import login_required
+from . import forms
 
 def article_list(request):
     articles = Article.objects.all().order_by('date')
@@ -14,4 +15,10 @@ def article_detail(request, slug):
 
 @login_required(login_url='/accounts/login/')
 def article_create(request):
-    return render(request, 'articles/article_create.html')
+    if request.method == 'POST':
+        form = forms.CreateArticle(request.POST, request.FILES)
+        if form.is_valid():
+            return redirect('articles:list')
+    else:
+        form = forms.CreateArticle()
+    return render(request, 'articles/article_create.html', {'form':form})
